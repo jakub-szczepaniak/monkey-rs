c4469ed9b6bc35b8c13c6a959bc0c54d262dbf29
Jakub Szczepaniak <jakub.szczepaniak@heyjobs.de>
2024-07-06 10:21:17 +0200

    Add repl and command line application

---
+++ src/bin/main.rs
@@ -0,0 +1,46 @@
+use std::{io::{self, BufRead, Write}, path::PathBuf};
+
+use lexer::Lexer;
+use monkey_rs::*;
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about=None)]
+struct Cli {
+    filename: Option<PathBuf>
+}
+
+fn main() {
+    println!("Hello!");
+    let cli = Cli::parse();
+    if let Some(filename) = cli.filename {
+        todo!("Not there yet");
+    } else {
+        println!("Starting Monkey REPL");
+        repl()
+    }
+}
+
+fn repl() {
+    let stdin = io::stdin();
+    print!("Monkey>>");
+    let _ = io::stdout().flush();
+    for line in stdin.lock().lines() {
+        
+        if let Ok(line) = line {
+            if line.is_empty() {
+                break;
+            }
+            let mut lexer = Lexer::new(&line);
+            loop {
+                let token = lexer.next_token();
+                if token.is_eof() || token.is_illegal() {
+                    break;
+                }
+                println!("{}",token);
+            } 
+        }
+        print!(">>");
+        let _ = io::stdout().flush();
+    }
+}
