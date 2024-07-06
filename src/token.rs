e02799298d9108936bf1a7c1d4ac3ec5ad6f7786
Jakub Szczepaniak <jakub.szczepaniak@heyjobs.de>
2024-07-06 10:21:49 +0200

    Add display implementation for token and tokentype

--- src/token.rs
+++ src/token.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[derive(Debug, PartialEq)]
 pub struct Token {
     ttype: TokenType,
@@ -39,6 +41,12 @@ pub enum TokenType {
     IllegalToken,
 }
 
+impl Display for TokenType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "hell")
+    }
+}
+
 impl Token {
     pub fn new(ttype: TT, lexeme: String) -> Self {
         Self { ttype, lexeme }
@@ -52,5 +60,19 @@ impl Token {
     }
     pub fn from_str(ttype: TT, lexeme: &str) -> Self {
         Self::new(ttype, lexeme.to_string())
+    }
+
+    pub fn is_illegal(&self) -> bool {
+        self.ttype == TT::IllegalToken
+    }
+
+    pub fn is_eof(&self) -> bool {
+        self.ttype == TT::Eof
+    }
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} - {}", self.ttype, self.lexeme)
     }
 }
